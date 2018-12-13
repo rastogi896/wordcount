9c2beb3a55f7fb5283848b70c241a5a5568304e4
rastogi896 <dk2832426@gmail.com>
2018-12-13 16:13:25 +0530

    first_commit

---
+++ wordcount/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def homepage(request):
+	return render(request,'home.html')
+def count(request):
+	data=request.GET['fulltextarea']
+	word_list=data.split()
+	list_length=len(word_list)
+	word_dictionary = {}
+	for word in word_list:
+		if word in word_dictionary:
+			word_dictionary[word] +=1
+		else:
+			word_dictionary[word] =1
+
+
+
+
+	return render(request,'count.html',{'fulltext':data,'words':list_length,'word_dictionary':word_dictionary})
+def profile(request):
+	return render(request,'profile.html')
